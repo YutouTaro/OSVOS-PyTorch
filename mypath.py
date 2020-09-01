5a9bd5791a124e0605720d6ff6a2db7178b8e0ed
yu <kxhyu17@gmail.com>
2020-09-01 13:56:26 +0800

    added backup path

--- mypath.py
+++ mypath.py
@@ -5,12 +5,14 @@ class Path(PathAbstract):
     @staticmethod
     def db_root_dir():
         return '/content/drive/My Drive/datasets/DAVIS'
+        # return '/home/yu/DataSet/DAVIS'
 
     @staticmethod
     def save_root_dir():
         return '/content/drive/My Drive/datasets/DAVIS/models'
+        # return '/home/yu/OSVOS-PyTorch/models'
 
     @staticmethod
     def models_dir():
         return "/content/drive/My Drive/datasets/DAVIS/models"
-
+        # return '/home/yu/OSVOS-PyTorch/models'
