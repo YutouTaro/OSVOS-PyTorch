8d60cebc28666426bbeff289f810f2b3fcfb3b6f
yu <kxhyu17@gmail.com>
2020-09-10 16:24:41 +0800

    modified for DAVIS2016

--- mypath.py
+++ mypath.py
@@ -4,15 +4,15 @@ from util.path_abstract import PathAbstract
 class Path(PathAbstract):
     @staticmethod
     def db_root_dir():
-        return '/content/drive/My Drive/datasets/DAVIS'
+        return '/content/drive/My Drive/datasets/PASCAL2010'
         # return '/home/yu/DataSet/DAVIS'
 
     @staticmethod
     def save_root_dir():
-        return '/content/drive/My Drive/datasets/DAVIS/models'
+        return '/content/drive/My Drive/datasets/PASCAL2010/models'
         # return '/home/yu/OSVOS-PyTorch/models'
 
     @staticmethod
     def models_dir():
-        return "/content/drive/My Drive/datasets/DAVIS/models"
+        return "/content/drive/My Drive/datasets/PASCAL2010/models"
         # return '/home/yu/OSVOS-PyTorch/models'
